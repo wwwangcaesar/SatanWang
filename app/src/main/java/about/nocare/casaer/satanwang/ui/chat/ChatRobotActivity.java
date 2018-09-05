7e682b8f005cd157c2d3f82d604ce4c95f0faecf
wangzhuowei <wangzhuowei@lovcreate.com>
2018-09-05 14:43:46 +0800

    添加百度语音识别对话框相关内容 添加百度语音识别对话框jar包相关依赖uidialog 将原来放置在app中的bdasr.jar包放到core中，以方便uidialog调用 在core包中添加百度listener相关语音输入监听，其中MyRecognizer为用户自定义Listener

--- app/src/main/java/about/nocare/casaer/satanwang/ui/chat/ChatRobotActivity.java
+++ app/src/main/java/about/nocare/casaer/satanwang/ui/chat/ChatRobotActivity.java
@@ -30,11 +30,18 @@ import com.baidu.speech.EventListener;
 import com.baidu.speech.EventManager;
 import com.baidu.speech.EventManagerFactory;
 import com.baidu.speech.asr.SpeechConstant;
+import com.baidu.voicerecognition.android.ui.BaiduASRDigitalDialog;
+import com.baidu.voicerecognition.android.ui.DigitalDialogInput;
 import com.google.gson.Gson;
 import com.jaeger.library.StatusBarUtil;
+import com.lovcreate.core.baidu.MyRecognizer;
+import com.lovcreate.core.baidu.listener.ChainRecogListener;
+import com.lovcreate.core.baidu.listener.IRecogListener;
+import com.lovcreate.core.baidu.listener.MessageStatusRecogListener;
 import com.lovcreate.core.base.BaseActivity;
 import com.lovcreate.core.base.OnClickListener;
 import com.lovcreate.core.util.StringUtil;
+import com.lovcreate.core.util.ToastUtil;
 
 import org.json.JSONObject;
 
@@ -90,6 +97,12 @@ public class ChatRobotActivity extends BaseActivity implements EventListener {
     private ChatMessageAdapter msgAdapter;
     /*百度语音识别*/
     private EventManager asr;
+    /*百度语音识别对话框*/
+    private DigitalDialogInput input;
+    private ChainRecogListener chainRecogListener;
+    protected boolean running = false;
+    private MyRecognizer myRecognizer;
+    protected Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -115,9 +128,38 @@ public class ChatRobotActivity extends BaseActivity implements EventListener {
         loadApps();
         initPermission();
         initView();
-        /*百度语音识别*/
+        /*百度语音精简版识别*/
+
         asr = EventManagerFactory.create(this, "asr");
         asr.registerListener(this); //  EventListener 中 onEvent方法
+
+
+        /*百度语音识别对话框*/
+        //handler 用于实时返回数据，检测每秒的语音输入情况，同事返回相应的处理结果
+        // 如果想看某些具体运行过程可以在这里设置
+        handler = new Handler() {
+
+            /*
+             * @param msg
+             */
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+
+//                ToastUtil.showToastBottomShort(msg.obj.toString()+"+++handler");
+
+            }
+
+        };
+        // DEMO集成步骤 1.1 新建一个回调类，识别引擎会回调这个类告知重要状态和识别结果
+        IRecogListener listener = new MessageStatusRecogListener(handler);
+        // DEMO集成步骤 1.2 初始化：new一个IRecogListener示例 & new 一个 MyRecognizer 示例
+        myRecognizer = new MyRecognizer(this, listener);
+        chainRecogListener = new ChainRecogListener();
+        // DigitalDialogInput 输入 ，MessageStatusRecogListener可替换为用户自己业务逻辑的listener
+        chainRecogListener.addListener(new MessageStatusRecogListener(handler));
+        myRecognizer.setEventListener(chainRecogListener); // 替换掉原来的listener
+
         initData();
         initEventListen();//监听，点击事件
     }
@@ -220,9 +262,10 @@ public class ChatRobotActivity extends BaseActivity implements EventListener {
         btnPressToSpeak.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
-                Vibrator vibrator = (Vibrator)ChatRobotActivity.this.getSystemService(ChatRobotActivity.this.VIBRATOR_SERVICE);
+                Vibrator vibrator = (Vibrator) ChatRobotActivity.this.getSystemService(ChatRobotActivity.this.VIBRATOR_SERVICE);
                 vibrator.vibrate(30);
                 start();
+//                mystart();
                 return false;
             }
         });
@@ -295,8 +338,27 @@ public class ChatRobotActivity extends BaseActivity implements EventListener {
         msgAdapter.notifyDataSetChanged();
     }
 
-    //启动百度语音TTs
+    //启动百度语音弹框
     private void start() {
+        // 此处params可以打印出来，直接写到你的代码里去，最终的json一致即可。
+        Map<String, Object> params = new LinkedHashMap<String, Object>();
+        String event = null;
+        event = SpeechConstant.ASR_START; // 替换成测试的event
+        params.put(SpeechConstant.ACCEPT_AUDIO_VOLUME, false);
+        // BaiduASRDigitalDialog的输入参数
+        input = new DigitalDialogInput(myRecognizer, chainRecogListener, params);
+        BaiduASRDigitalDialog.setInput(input); // 传递input信息，在BaiduASRDialog中读取,
+        Intent intent = new Intent(this, BaiduASRDigitalDialog.class);
+
+        // 修改对话框样式
+        // intent.putExtra(BaiduASRDigitalDialog.PARAM_DIALOG_THEME, BaiduASRDigitalDialog.THEME_ORANGE_DEEPBG);
+
+        running = true;
+        startActivityForResult(intent, 2);
+    }
+
+    //启动百度语音TTs
+    private void mystart() {
         Map<String, Object> params = new LinkedHashMap<String, Object>();
         String event = null;
         event = SpeechConstant.ASR_START; // 替换成测试的event
@@ -324,8 +386,6 @@ public class ChatRobotActivity extends BaseActivity implements EventListener {
         json = new JSONObject(params).toString(); // 这里可以替换成你需要测试的json
         asr.send(event, json, null, 0, 0);
     }
-
-    private String realut;
 
     // 百度语音的接口回调事件
     @Override
@@ -374,6 +434,27 @@ public class ChatRobotActivity extends BaseActivity implements EventListener {
     }
 
     @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        running = false;
+        if (requestCode == 2) {
+            String message = "对话框的识别结果：";
+            if (resultCode == RESULT_OK) {
+                ArrayList results = data.getStringArrayListExtra("results");
+                if (results != null && results.size() > 0) {
+                    // results.get(0) 即是返回的最佳结果
+                    MessageEntity entity = new MessageEntity(ChatMessageAdapter.TYPE_RIGHT, TimeUtil.getCurrentTimeMillis(), results.get(0).toString());
+                    msgList.add(entity);
+                    msgAdapter.notifyDataSetChanged();
+                    requestApiByRetrofit_RxJava(results.get(0).toString());
+                }
+            } else {
+                ToastUtil.showToastBottomShort("识别失败，请使用普通话");
+            }
+        }
+
+    }
+
+    @Override
     protected void onDestroy() {
         super.onDestroy();
         asr.send(SpeechConstant.ASR_CANCEL, "{}", null, 0, 0);
@@ -386,6 +467,10 @@ public class ChatRobotActivity extends BaseActivity implements EventListener {
         super.onPause();
         asr.send(SpeechConstant.ASR_CANCEL, "{}", null, 0, 0);
         Log.i("ActivityMiniRecog", "On pause");
+        if (!running) {
+            myRecognizer.release();
+            finish();
+        }
     }
 
 }
